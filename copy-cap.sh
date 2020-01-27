354688f8762426d88fc6f7ea88fc369bd21346d3
Florian Salord <florian.salord@etu.univ-cotedazur.fr>
2020-01-27 21:48:58 +0100

    Fix folder removal only if exists

--- copy-cap.sh
+++ copy-cap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -r ./tfc-cap-updater
+[[ -d ./tfc-cap-updater ]] && rm -r ./tfc-cap-updater
 mkdir -p ./tfc-cap-updater
 cp -r ../tfc-cap-updater/src/*.go ./tfc-cap-updater/
 cp -r ../tfc-cap-updater/src/go.* ./tfc-cap-updater/
